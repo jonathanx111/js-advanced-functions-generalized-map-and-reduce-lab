47f05007f594537040bfec08557e8499563d1828
jonathanx111 <jonathanx111@gmail.com>
2020-12-21 22:52:13 -0500

    commit tree 71971648d9d20bc8eb56ba2d1a7030c706e128b9

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your functions here
+function map(array, cb) {
+    let n = []
+
+    for (let i=0; i < array.length; i++) {
+        n.push(cb(array[i]))
+    }
+
+    return n
+}
+
+function reduce(array, cb, startingValue) {
+    let n = (!!startingValue) ? startingValue : array[0]
+    let i = (!!startingValue) ? 0 : 1
+
+    for (; i < array.length; i++) {
+        n = cb(array[i], n)
+    }
+
+
+
+    return n
+}
